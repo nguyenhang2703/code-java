3fcb7de4d9e0b424153759ffa76012947dcb73a9
nguyenhang2703 <125906239+nguyenhang2703@users.noreply.github.com>
2023-02-21 00:01:31 +0700

    Update NguyenThiHang_App.java

--- NguyenThiHang_App.java
+++ NguyenThiHang_App.java
@@ -1,4 +1,4 @@
-public class NguyenDuyThai_App {
+public class NguyenThiHang_App {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hằng đang học Java");
